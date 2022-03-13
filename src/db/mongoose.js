05fee46531aed63f2fb9a08dcb2655e77a522577
ragog <giovanni@checklyhq.com>
2022-03-13 18:41:08 +0100

    feat: external db connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
 const dbConnectionURL = process.env.DB_URL || "mongodb://127.0.0.1:27017";
-const databasePath = "/restore";
+const dbPath = "/restore_db";
 
-mongoose.connect(dbConnectionURL + databasePath, {
+mongoose.connect(dbConnectionURL + dbPath, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useFindAndModify: false,
-  // useCreateIndex: true,
 });
 
 module.exports = mongoose;
