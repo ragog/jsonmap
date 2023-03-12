d1e1bafec6ec3caef2340fecb6d82f6caa0007f8
ragog <giovanni@checklyhq.com>
2023-03-12 15:40:34 +0100

    feat: return item content only

--- src/route/items.js
+++ src/route/items.js
@@ -1,41 +1,37 @@
-const express = require("express");
-const Item = require("../model/item.js");
-const User = require("../model/user.js")
+const express = require('express');
+const Item = require('../model/item.js');
+const User = require('../model/user.js');
 const router = new express.Router();
-const preExec = require("../middleware/middleware.js");
+const preExec = require('../middleware/middleware.js');
 const uuid = require('uuid');
-const crypto = require('crypto')
-const getHashFromRequest = require('../helper/helper.js')
+const getHashFromRequest = require('../helper/helper.js');
 
 router.use(preExec);
 
 router.get('/v1/items', async (req, res) => {
+	const hashedToken = getHashFromRequest(req);
 
-	const hashedToken = getHashFromRequest(req)
+	const ownerUser = await User.findOne({ apikey: hashedToken });
 
-	const ownerUser = await User.findOne({ apikey: hashedToken })
-
-	const items = await Item.find({ owner: ownerUser.id })
-	const formatItems = items.map((x) => ({ key: x.key, value: x.value }))
+	const items = await Item.find({ owner: ownerUser.id });
+	const formatItems = items.map((x) => ({ key: x.key, value: x.value }));
 
 	res.send(formatItems);
-
 });
 
 router.post('/v1/items', async (req, res) => {
-	
 	if (!req.body.key) {
-		req.body.key = uuid.v4()
+		req.body.key = uuid.v4();
 	}
 
-    const hashedToken = getHashFromRequest(req)
+	const hashedToken = getHashFromRequest(req);
 
-	const ownerUser = await User.findOne({ apikey: hashedToken })
+	const ownerUser = await User.findOne({ apikey: hashedToken });
 
-	const existingItemWithKey = await Item.findOne({ key: req.body.key })
+	const existingItemWithKey = await Item.findOne({ key: req.body.key });
 	if (existingItemWithKey) {
-		res.status(400).send('Bad request: key already in use')
-		return
+		res.status(400).send('Bad request: key already in use');
+		return;
 	}
 
 	try {
@@ -43,58 +39,53 @@ router.post('/v1/items', async (req, res) => {
 			id: uuid.v4(),
 			key: req.body.key,
 			value: req.body.value,
-			owner: ownerUser.id
+			owner: ownerUser.id,
 		}).save();
 	} catch (error) {
-		res.status(400).send('Bad request: '+ error)
-		return
+		res.status(400).send('Bad request: ' + error);
+		return;
 	}
-	
-	res.send(req.body.key)
-	
+
+	res.send(req.body.key);
 });
 
 router.get('/v1/items/:key', async (req, res) => {
-
 	if (!req.params.key) {
-		res.status(400).send('Bad request')
-		return
+		res.status(400).send('Bad request');
+		return;
 	}
 
-	const hashedToken = getHashFromRequest(req)
+	const hashedToken = getHashFromRequest(req);
 
-	const ownerUser = await User.findOne({ apikey: hashedToken })
+	const ownerUser = await User.findOne({ apikey: hashedToken });
 
-	const item = await Item.findOne({ key: req.params.key })
+	const item = await Item.findOne({ key: req.params.key });
 	if (item?.owner === ownerUser?.id) {
-		res.send({ key: item.key, value: item.value });
-		return
+		res.send(item.value);
+		return;
 	}
 
-	res.status(404).send("No such item found")
-
+	res.status(404).send('No such item found');
 });
 
 router.delete('/v1/items/:key', async (req, res) => {
-
 	if (!req.params.key) {
-		res.status(400).send('Bad request')
-		return
+		res.status(400).send('Bad request');
+		return;
 	}
 
-	const hashedToken = getHashFromRequest(req)
+	const hashedToken = getHashFromRequest(req);
 
-	const ownerUser = await User.findOne({ apikey: hashedToken })
+	const ownerUser = await User.findOne({ apikey: hashedToken });
 
-	const item = await Item.findOne({ key: req.params.key })
+	const item = await Item.findOne({ key: req.params.key });
 	if (item?.owner === ownerUser?.id) {
-		await Item.deleteOne({ key: req.params.key })
-		res.status(204).send()
-		return
+		await Item.deleteOne({ key: req.params.key });
+		res.status(204).send();
+		return;
 	}
 
-	res.status(404).send("No such item found")
-
+	res.status(404).send('No such item found');
 });
 
 module.exports = router;
