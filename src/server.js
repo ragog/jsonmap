578d6fd458fc33c2a0adaf32d83834a6803fade3
ragog <giovanni@checklyhq.com>
2025-04-23 22:37:31 +0200

    feat: prometheus setup

--- src/server.js
+++ src/server.js
@@ -1,17 +1,46 @@
 const express = require('express');
-const itemsRouter = require("./route/items.js");
-const usersRouter = require("./route/users.js");
-const forceHTTPS = require("./middleware/https.js")
+const itemsRouter = require('./route/items.js');
+const usersRouter = require('./route/users.js');
+const forceHTTPS = require('./middleware/https.js');
 const path = require('path');
-require('./db/mongoose')
+const prometheus = require('prom-client');
+require('./db/mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
-if (process.env.DB_URL) { app.use(forceHTTPS) }
-app.use('/api', itemsRouter)
-app.use('/api', usersRouter)
+// Prometheus monitoring setup
+const register = new prometheus.Registry();
+prometheus.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new prometheus.Counter({
+	name: 'http_requests_total',
+	help: 'Total number of HTTP requests',
+	labelNames: ['method', 'route', 'status_code'],
+});
+register.registerMetric(httpRequestCounter);
+
+// Middleware to count requests for prometheus
+app.use((req, res, next) => {
+	res.on('finish', () => {
+		httpRequestCounter.labels(req.method, req.route?.path || req.path, res.statusCode).inc();
+	});
+	next();
+});
+
+// Serve metrics at /metrics for prometheus
+app.get('/metrics', async (req, res) => {
+	res.set('Content-Type', register.contentType);
+	res.end(await register.metrics());
+});
+
+app.use(express.json());
+if (process.env.DB_URL) {
+	app.use(forceHTTPS);
+}
+
+app.use('/api', itemsRouter);
+app.use('/api', usersRouter);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/static/index.html'));
